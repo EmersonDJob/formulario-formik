54cf8d74f1e7d71f6f182e002635c62ede6ef5fa
EmersonDjob <jobbr70@gmail.com>
2021-07-15 23:59:53 -0300

    background gradient

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Cadastrar } from './components/Cadastrar';
+import styled from 'styled-components';
+
 
 
 function App() {
   return (
-   <>
+   <Container>
   
    <Cadastrar />
-   </>
+   </Container>
   );
 }
 
 export default App;
+
+const Container  = styled.div`
+height: 100vh;
+width: 100vw;
+background-image: linear-gradient(-20deg, #00cdac 0%, #8ddad5 100%);
+`
